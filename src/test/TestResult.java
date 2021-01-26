18515fe107c8655b9c3393471e6ed3f003c8a372
Hizzze <75045730+Hizzze@users.noreply.github.com>
2021-01-26 14:01:16 +0200

    Add files via upload

---
+++ src/test/TestResult.java
@@ -0,0 +1,15 @@
+package test;
+
+import domain.Exercise;
+
+public class TestResult {
+
+    public static void main(String[] args) {
+	public static void main(String[] args) {
+        public static void main(String[] args) {
+        String text = "Do geese see God";
+            System.out.println("Whether the `" + text + "` is a palindrome: " + Exercise.Calculate(text));
+    }
+    }
+    }
+}
